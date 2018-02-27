53327a03b21a8750d8ecbe114e994e9df6b311ec
androidyanghuan <435025168@qq.com>
2018-02-27 16:54:42 +0800

    new package and new class

---
+++ sdk/src/main/java/cn/sn/zwcx/sdk/config/DBConfig.java
@@ -0,0 +1,20 @@
+package cn.sn.zwcx.sdk.config;
+
+/**
+ * Created by on 2018/1/10 11:22.
+ * Created by author YangHuan
+ * e-mail:435025168@qq.com
+ * 数据库全局常量
+ */
+
+public class DBConfig {
+    /** 数据库名称 */
+    public static final String DB_NAME = "db_name_yizhi";
+    /** 表名 */
+    public static final String TABLE_ZHIHU = "table_zhihu";
+    public static final String TABLE_WANGYI = "table_top_news";
+    public static final String TABLE_WEIXIN = "table_weixin";
+    public static final String TABLE_GANKIO_DAY = "table_gank_io_day";
+    public static final String TABLE_GANKIO_CUSTOM = "table_gank_io_custom";
+
+}
---
+++ sdk/src/main/java/cn/sn/zwcx/sdk/config/ItemState.java
@@ -0,0 +1,20 @@
+package cn.sn.zwcx.sdk.config;
+
+/**
+ * Created by on 2018/1/10 11:56.
+ * Created by author YangHuan
+ * e-mail:435025168@qq.com
+ * item状态全局常量
+ */
+
+public class ItemState {
+    /**
+     * 已读状态
+     */
+    public static final int STATE_IS_READ = 1;
+
+    /**
+     * 非已读状态
+     */
+    public static final int STATE_IS_NO_READ = 0;
+}
